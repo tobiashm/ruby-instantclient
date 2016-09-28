a6eb7a086dc420c152268a2dc6a45ea10f556e41
Tobias Haagen Michaelsen <tobias.michaelsen@karnovgroup.com>
2016-09-28 23:14:47 +0200

    Add Instant Client 12.1

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ oracle_versions=( vendor/* )
 ruby_versions=( 2.*/ )
 for ruby_version in "${ruby_versions[@]%/}"; do
   for oracle_version in "${oracle_versions[@]#vendor/}"; do
-    docker build . -f $ruby_version/Dockerfile -t "tobiashm/ruby-instantclient:$ruby_version-$oracle_version"
+    docker build . -f $ruby_version/$oracle_version/Dockerfile -t "tobiashm/ruby-instantclient:$ruby_version-$oracle_version"
   done
 done
--- update.sh
+++ update.sh
@@ -4,10 +4,11 @@ oracle_versions=( vendor/* )
 ruby_versions=( 2.*/ )
 for ruby_version in "${ruby_versions[@]%/}"; do
   for oracle_version in "${oracle_versions[@]#vendor/}"; do
+    mkdir -p "$ruby_version/$oracle_version"
     sedStr="
       s!%%ORACLE_VERSION%%!$oracle_version!g;
       s!%%RUBY_VERSION%%!$ruby_version!g;
     "
-    sed -e "$sedStr" "Dockerfile.template" > "$ruby_version/Dockerfile"
+    sed -e "$sedStr" "Dockerfile.template" > "$ruby_version/$oracle_version/Dockerfile"
   done
 done
