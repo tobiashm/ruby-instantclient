b440b680d69148642ba2771dafd24ab77c68546d
Tobias Haagen Michaelsen <tobias.michaelsen@karnovgroup.com>
2016-09-28 23:37:28 +0200

    Reorganized to have Dockerfiles in root
    To enable automatic builds on Docker Hub to find the vendor files

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 set -e
 oracle_versions=( vendor/* )
-ruby_versions=( 2.*/ )
-for ruby_version in "${ruby_versions[@]%/}"; do
+while read ruby_version; do
   for oracle_version in "${oracle_versions[@]#vendor/}"; do
-    docker build . -f $ruby_version/$oracle_version/Dockerfile -t "tobiashm/ruby-instantclient:$ruby_version-$oracle_version"
+    docker build . -f "Dockerfile-$ruby_version-$oracle_version" -t "tobiashm/ruby-instantclient:$ruby_version-$oracle_version"
   done
-done
+done < ruby-versions
--- update.sh
+++ update.sh
@@ -1,14 +1,12 @@
 #/bin/bash
 set -e
 oracle_versions=( vendor/* )
-ruby_versions=( 2.*/ )
-for ruby_version in "${ruby_versions[@]%/}"; do
+while read ruby_version; do
   for oracle_version in "${oracle_versions[@]#vendor/}"; do
-    mkdir -p "$ruby_version/$oracle_version"
     sedStr="
       s!%%ORACLE_VERSION%%!$oracle_version!g;
       s!%%RUBY_VERSION%%!$ruby_version!g;
     "
-    sed -e "$sedStr" "Dockerfile.template" > "$ruby_version/$oracle_version/Dockerfile"
+    sed -e "$sedStr" "Dockerfile.template" > "Dockerfile-$ruby_version-$oracle_version"
   done
-done
+done < ruby-versions
