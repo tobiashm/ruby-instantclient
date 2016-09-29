f7720243488e437b466e6e46d090ef0af03b7c16
Tobias Haagen Michaelsen <tobias.michaelsen@karnovgroup.com>
2016-09-29 11:33:03 +0200

    Make images with both basic and basiclite client

--- update.sh
+++ update.sh
@@ -3,10 +3,13 @@ set -e
 oracle_versions=( vendor/* )
 while read ruby_version; do
   for oracle_version in "${oracle_versions[@]#vendor/}"; do
-    sedStr="
-      s!%%ORACLE_VERSION%%!$oracle_version!g;
-      s!%%RUBY_VERSION%%!$ruby_version!g;
-    "
-    sed -e "$sedStr" "Dockerfile.template" > "Dockerfile-$ruby_version-$oracle_version"
+    for package in basic basiclite; do
+      sedStr="
+        s!%%ORACLE_VERSION%%!$oracle_version!g;
+        s!%%PACKAGE%%!$package!g
+        s!%%RUBY_VERSION%%!$ruby_version!g;
+      "
+      sed -e "$sedStr" "Dockerfile.template" > "Dockerfile-$ruby_version-$oracle_version-$package"
+    done
   done
 done < ruby-versions
