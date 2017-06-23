eb1277808b533aefdff230e66be196a426f9dbbb
Tobias Haagen Michaelsen <tobias.michaelsen@karnovgroup.com>
2017-06-23 10:58:27 +0200

    Update build script
    Fix script to mach that we now have both ‘basic’ and ‘basiclite’ images.
    
    Optional push to Docker Hub after build.

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@ set -e
 oracle_versions=( vendor/* )
 while read ruby_version; do
   for oracle_version in "${oracle_versions[@]#vendor/}"; do
-    docker build . -f "Dockerfile-$ruby_version-$oracle_version" -t "tobiashm/ruby-instantclient:$ruby_version-$oracle_version"
+    version="$ruby_version-$oracle_version"
+    for variant in basic basiclite; do
+      tag="tobiashm/ruby-instantclient:$version-$variant"
+      docker build . -f "Dockerfile-$version-$variant" -t "$tag"
+      if [ "$PUSH" ]; then docker push "$tag"; fi
+    done
   done
 done < ruby-versions
