920dbf58b6b4b2c0bdc567e47ff4bd13664c3220
Tobias Haagen Michaelsen <tobias.michaelsen@gmail.com>
2019-05-14 14:21:28 +0200

    Output to logfile
    Build script will report simlified progress, and redirect full Dcoker build log to a file.

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 oracle_versions=( vendor/* )
+logfile="log/build-$(date "+%Y%m%d%H%M%S").log"
+touch "$logfile"
 while read ruby_version; do
   for oracle_version in "${oracle_versions[@]#vendor/}"; do
     version="$ruby_version-$oracle_version"
     for variant in basic basiclite; do
       tag="tobiashm/ruby-instantclient:$version-$variant"
-      docker build . -f "Dockerfile-$version-$variant" -t "$tag"
-      if [ "$PUSH" ]; then docker push "$tag"; fi
+      echo "Building $version-$variant ..."
+      docker build . -f "Dockerfile-$version-$variant" -t "$tag" 2>&1 >> "$logfile"
+      if [ "$PUSH" ]; then docker push "$tag" 2>&1 >> "$logfile"; fi
       if [ "$variant" == "basiclite" ]; then
         default_tag="${tag%-$variant}"
-        docker tag "$tag" "$default_tag"
-        if [ "$PUSH" ]; then docker push "$default_tag"; fi
+        docker tag "$tag" "$default_tag" 2>&1 >> "$logfile"
+        if [ "$PUSH" ]; then docker push "$default_tag" 2>&1 >> "$logfile"; fi
       fi
     done
   done
